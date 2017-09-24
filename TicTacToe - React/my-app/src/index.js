cdbb345506ef9f1e416150940cc0ce163b33fe61
niki budinov <nbudinov@siriushome.com>
2017-09-24 10:31:01 +0300

    Added next player logic; Added winner logic

--- TicTacToe - React/my-app/src/index.js
+++ TicTacToe - React/my-app/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
 
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onclick()}>
-        { this.props.value }
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onclick()}>
+//         { this.props.value }
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onclick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -18,48 +27,59 @@ class Board extends React.Component {
         super();
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
-  renderSquare(i) {
-    return (
-         <Square
-            value={this.state.squares[i]}
-            onclick={() => this.handleClick(i)}
-        />
-    )
-  }
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onclick={() => this.handleClick(i)}
+            />
+        )
+    }
 
     handleClick(i) {
-      const squares = this.state.squares.slice();
-      console.log(squares);
-      squares[i] = 'X';
-      this.setState({squares: squares});
-    }
-  render() {
-    const status = 'Next player: X';
+        const squares = this.state.squares.slice();
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+        squares[i] = (this.state.xIsNext ? "X" : "O");
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = "Winner is: " + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+        }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 class Game extends React.Component {
@@ -84,3 +104,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (var i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] !== null && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
